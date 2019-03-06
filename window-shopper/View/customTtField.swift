5c5b25afc173f94f26df91299574751c5558d33c
Anonymous <kidooe@gmail.com>
2019-03-06 12:28:25 -0800

    first lesson done

---
+++ window-shopper/View/customTtField.swift
@@ -0,0 +1,27 @@
+//
+//  customTtField.swift
+//  window-shopper
+//
+//  Created by Anonymous on 06/03/2019.
+//  Copyright © 2019 Anonymous. All rights reserved.
+//
+
+import UIKit
+
+class customTtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p , attributes:[.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
